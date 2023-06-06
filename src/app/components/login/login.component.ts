809024187b1c46b696695b15181f01cb4359aa13
Pum2A <epros98@gmail.com>
2023-06-06 20:41:37 +0200

    login form added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormBuilder}   from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,33 @@ import { FormBuilder}   from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private fb: FormBuilder){
 
+  loginForm: any = {
+    username: '',
+    password: ''
+  };
+
+
+  login() {
+    const storedUserDataString = localStorage.getItem('userData');
+    if (storedUserDataString !== null) {
+      const storedUserData = JSON.parse(storedUserDataString);
+      const enteredUsername = this.loginForm.username;
+      const enteredPassword = this.loginForm.password;
+
+      if (
+        enteredUsername === storedUserData.username &&
+        enteredPassword === storedUserData.password
+      ) {
+        console.log('Zalogowano pomyślnie!');
+        // operacje po zalogowaniu, np. przekierować użytkownika na inną stronę.
+      } else {
+        console.log('Nieprawidłowe dane logowania!');
+        // komunikat o błędzie logowania.
+      }
+    } else {
+      console.log('Brak danych użytkownika w LocalStorage!');
+      // komunikat o braku zarejestrowanych użytkowników.
+    }
   }
-
 }
